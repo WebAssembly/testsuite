7f354b3368b4492522aca2458a85dbfb76136199
Ben Smith <binji@chromium.org>
2018-02-05 15:38:18 -0800

    Merge proposal repos with spec repo first

--- update-testsuite.sh
+++ update-testsuite.sh
@@ -6,32 +6,87 @@ set -o pipefail
 repos='spec threads simd exception-handling gc bulk-memory-operations tail-call nontrapping-float-to-int-conversions multi-value host-bindings sign-extension-ops mutable-global'
 
 log_and_run() {
-    echo $*
+    echo ">>" $*
     if ! $*; then
         echo "sub-command failed: $*"
         exit
     fi
 }
 
+try_log_and_run() {
+    echo ">>" $*
+    $*
+}
+
+pushdir() {
+    pushd $1 >/dev/null || exit
+}
+
+popdir() {
+    popd >/dev/null || exit
+}
+
 update_repo() {
     local repo=$1
-    pushd repos >/dev/null
+    pushdir repos
         if [ -d ${repo} ]; then
-            pushd ${repo} >/dev/null
-                log_and_run git fetch origin
-                log_and_run git reset origin/master --hard
-            popd >/dev/null
+            log_and_run git -C ${repo} fetch origin
+            log_and_run git -C ${repo} reset origin/master --hard
         else
             log_and_run git clone https://github.com/WebAssembly/${repo}
         fi
-    popd >/dev/null
+
+        # Add upstream spec as "spec" remote.
+        if [ "${repo}" != "spec" ]; then
+            pushdir ${repo}
+                if ! git remote | grep spec >/dev/null; then
+                    log_and_run git remote add spec https://github.com/WebAssembly/spec
+                fi
+
+                log_and_run git fetch spec
+            popdir
+        fi
+    popdir
 }
 
+merge_with_spec() {
+    local repo=$1
+
+    [ "${repo}" == "spec" ] && return
+
+    pushdir repos/${repo}
+        # Create and checkout "try-merge" branch.
+        if ! git branch | grep try-merge >/dev/null; then
+            log_and_run git branch try-merge origin/master
+        fi
+        log_and_run git checkout try-merge
+
+        # Attempt to merge with spec/master.
+        log_and_run git reset origin/master --hard
+        try_log_and_run git merge -q spec/master -m "merged"
+        if [ $? -ne 0 ]; then
+            git merge --abort
+            popdir
+            return 1
+        fi
+    popdir
+    return 0
+}
+
+
 echo -e "update repos\n" > commit_message
+
+failed_repos=
 
 for repo in ${repos}; do
     echo "++ updating ${repo}"
     update_repo ${repo}
+
+    if ! merge_with_spec ${repo}; then
+        echo -e "!! error merging ${repo}, skipping\n"
+        failed_repos="${failed_repos} ${repo}"
+        continue
+    fi
 
     if [ "${repo}" = "spec" ]; then
         wast_dir=.
@@ -40,20 +95,20 @@ for repo in ${repos}; do
         wast_dir=proposals/${repo}
         mkdir -p ${wast_dir}
 
-        # Don't add old versions of spec tests.
-        pushd repos/${repo} >/dev/null
+        # Don't add tests from propsoal that are the same as spec.
+        pushdir repos/${repo}
             for new in test/core/*.wast; do
-                sha=$(git hash-object ${new})
-                if ! git -C ../../repos/spec cat-file -t ${sha} >/dev/null 2>&1; then
+                old=../../repos/spec/${new}
+                if [[ ! -f ${old} ]] || ! diff ${old} ${new} >/dev/null; then
                     log_and_run cp ${new} ../../${wast_dir}
                 fi
             done
-        popd >/dev/null
+        popdir
     fi
 
     # Check whether any files were updated.
     if [ $(git status -s ${wast_dir}/*.wast | wc -l) -ne 0 ]; then
-        git add ${wast_dir}/*.wast
+        log_and_run git add ${wast_dir}/*.wast
 
         repo_sha=$(git -C repos/${repo} log --max-count=1 --pretty=oneline | sed -e 's/ .*//')
         echo "  ${repo}: ${repo_sha}" >> commit_message
@@ -62,7 +117,11 @@ for repo in ${repos}; do
     echo -e "-- ${repo}\n"
 done
 
-git commit -a -F commit_message
+# git commit -a -F commit_message
 # git push
 
 echo "done"
+
+if [ -n "${failed_repos}" ]; then
+  echo "!! failed to update repos: ${failed_repos}"
+fi
